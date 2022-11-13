c7237e03d987a8286ddd078434c4e58bc1dddbf2
vladislavbystrov <keocera@icloud.com>
2022-11-13 18:52:15 +0300

    lesson 7. DI with setter

--- src/main/java/ru/vladbstrv/springcourse/TestSpring.java
+++ src/main/java/ru/vladbstrv/springcourse/TestSpring.java
@@ -8,11 +8,11 @@ public class TestSpring {
           "applicationContext.xml"
         );
 
-//        Music music = context.getBean("musicBean", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
-
         MusicPlayer musicPlayer = context.getBean("musicPlayerBean", MusicPlayer.class);
         musicPlayer.playMusic();
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }

6d1310a2b66cc5bc25b742fb63032f9f0c10b5c3
vladislavbystrov <keocera@icloud.com>
2022-11-14 12:58:11 +0300

    lesson 13. annotation @PostConstruct and @PreDestroy

--- src/main/java/ru/vladbstrv/springcourse/ClassicalMusic.java
+++ src/main/java/ru/vladbstrv/springcourse/ClassicalMusic.java
@@ -3,11 +3,23 @@ package ru.vladbstrv.springcourse;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
 public class ClassicalMusic implements Music{
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
+    }
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("init");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("destroy");
     }
 }

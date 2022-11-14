0c51191ed2b5beec5c2428459ad8775357765691
vladislavbystrov <keocera@icloud.com>
2022-11-14 12:39:21 +0300

    lesson 12. annotation @Qualifier

--- src/main/java/ru/vladbstrv/springcourse/MusicPlayer.java
+++ src/main/java/ru/vladbstrv/springcourse/MusicPlayer.java
@@ -1,19 +1,28 @@
 package ru.vladbstrv.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
+//    @Qualifier("rockMusic")
+    private Music music;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic) {
-        this.classicalMusic = classicalMusic;
+    public MusicPlayer(
+            @Qualifier("rockMusic") Music music
+    ) {
+        this.music = music;
     }
 
+//    @Qualifier("rockMusic")
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+
     public void playMusic() {
-        System.out.println("Playing: " + classicalMusic.getSong());
+        System.out.println("Playing: " + music.getSong());
     }
 
 }

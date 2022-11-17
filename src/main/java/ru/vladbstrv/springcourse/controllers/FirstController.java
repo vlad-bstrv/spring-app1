e8baf685d80f413def0e8c771de21e9497a5facc
vladislavbystrov <keocera@icloud.com>
2022-11-17 11:59:14 +0300

    lesson 20. Get arguments @RequestParam

--- src/main/java/ru/vladbstrv/springcourse/controllers/FirstController.java
+++ src/main/java/ru/vladbstrv/springcourse/controllers/FirstController.java
@@ -3,6 +3,7 @@ package ru.vladbstrv.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,12 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String getPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-
+    public String getPage(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "surname", required = false) String surname
+    ) {
         System.out.println("Hello " + name + " " + surname);
-
         return "first/hello";
     }
 
